c8630b626c8b3f03961232c4c6b6c261d0b401db
Hyuk <poiu03290@naver.com>
2025-09-08 16:26:41 +0900

    feat: LockButton 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { LockButton } from '@/components';
+import './App.css';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <main className="font-pretendard">
+      <LockButton isCompleted={false} title="Antonyms" />
+    </main>
+  );
 }
 
 export default App;
